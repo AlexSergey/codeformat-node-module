8547dac0b1461dd9330afb5d3f30a5f6a538641c
Serhii Aleksandrov <gooddev.sergey@gmail.com>
2022-07-20 19:35:49 +0300

    update

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import { helloWorld } from './message.js';
 
-const obj = { bar, baz: { foo: 'qux' } };
-
 const bootstrap = (): void => {
   // eslint-disable-next-line no-console
   console.log(helloWorld());
