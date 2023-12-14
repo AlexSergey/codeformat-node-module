5d9d11d10e52cfabb14c43eef65e9015ae7110b8
Serhii Aleksandrov <gooddev.sergey@gmail.com>
2023-12-14 09:34:42 +0200

    fix: fix eslint dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
+import { hideBin } from 'yargs/helpers';
+import yargs from 'yargs/yargs';
+
 import { helloWorld } from './message.js';
 
+const { argv } = yargs(hideBin(process.argv));
 const bootstrap = (): void => {
   // eslint-disable-next-line no-console
-  console.log(helloWorld());
+  console.log(helloWorld(), argv);
 };
 bootstrap();
