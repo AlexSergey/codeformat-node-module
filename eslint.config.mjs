673327030a1c04930a734c6e90aed25a79349a9e
Serhii Aleksandrov <gooddev.sergey@gmail.com>
2024-11-05 12:09:06 +0100

    feat: update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -52,11 +52,6 @@ const ignores = [
 ];
 const tsFiles = ['**/*.ts'];
 
-const defaultEnv = 'production';
-const supportedEnvs = ['development', 'production'];
-const currentEnv = supportedEnvs.includes(process.env.NODE_ENV) ? process.env.NODE_ENV : defaultEnv;
-const isDevelopment = currentEnv === 'development';
-
 const languageOptions = {
   ecmaVersion: 2024,
   globals: {
@@ -82,7 +77,7 @@ const customTypescriptConfig = {
     unicorn: eslintPluginUnicorn,
   },
   rules: {
-    '@typescript-eslint/ban-ts-comment': isDevelopment ? 'off' : 'error',
+    '@typescript-eslint/ban-ts-comment': 'error',
     '@typescript-eslint/ban-types': 'off',
     '@typescript-eslint/explicit-function-return-type': 'warn',
     '@typescript-eslint/naming-convention': [
@@ -106,16 +101,14 @@ const customTypescriptConfig = {
         allowSingleExtends: true,
       },
     ],
-    '@typescript-eslint/no-unused-vars': isDevelopment
-      ? 'off'
-      : [
-          'error',
-          {
-            args: 'after-used',
-            ignoreRestSiblings: false,
-            vars: 'all',
-          },
-        ],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        args: 'after-used',
+        ignoreRestSiblings: false,
+        vars: 'all',
+      },
+    ],
     '@typescript-eslint/return-await': 'off',
 
     camelcase: ['error', { properties: 'always' }],
@@ -139,10 +132,10 @@ const customTypescriptConfig = {
     'class-methods-use-this': 'off',
     'file-extension-in-import-ts/file-extension-in-import-ts': 'error',
     'newline-before-return': 'error',
-    'no-alert': isDevelopment ? 'off' : 'error',
+    'no-alert': 'error',
     'no-await-in-loop': 'off',
-    'no-console': isDevelopment ? 'off' : 'error',
-    'no-debugger': isDevelopment ? 'off' : 'error',
+    'no-console': 'error',
+    'no-debugger': 'error',
     'no-param-reassign': 'off',
     'no-plusplus': 'off',
     'no-return-await': 'off',
