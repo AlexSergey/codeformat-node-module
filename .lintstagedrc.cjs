25e88812e250615893caec1a697b6784a4817476
Serhii Aleksandrov <gooddev.sergey@gmail.com>
2024-11-22 11:19:49 +0100

    fix: fix small bugs

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   '*.md': ['prettier --write'],
   'package.json': ['npm run format:package'],
-  'src/**/*.{ts,json}': ['prettier --write', 'eslint --fix'],
   'src/**/*.ts': [() => 'npm run lint:ts'],
+  'src/**/*.{ts,json}': ['prettier --write', 'eslint --fix'],
 };
